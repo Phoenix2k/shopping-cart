9aee2a2b7887db7b795f3ceea2fe57ee5359aca6
Niko Pettersen <niko.pettersen@newthings.co>
2023-09-04 21:35:51 +0300

    Skip console logs in production

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,26 +1,32 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggerService {
   debug(...msg: any): void {
+    if (!isDevMode()) return;
     console.debug(...msg);
   }
   error(...msg: any): void {
+    if (!isDevMode()) return;
     console.error(...msg);
   }
   info(...msg: any): void {
+    if (!isDevMode()) return;
     console.info(...msg);
   }
   log(...msg: any): void {
+    if (!isDevMode()) return;
     console.log(...msg);
   }
   table(...msg: any): void {
+    if (!isDevMode()) return;
     console.table(...msg);
   }
   warn(...msg: any): void {
+    if (!isDevMode()) return;
     console.warn(...msg);
   }
 }
