2a20ac3c8fde85285ec6c77c0ecc0bbd26577bd6
Niko Pettersen <niko.pettersen@newthings.co>
2023-09-06 16:42:43 +0300

    Add cart count to checkout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, type OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   styleUrls: ['./header.component.css'],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnDestroy, OnInit {
+  private productSub: Subscription | null = null;
+
+  public amount = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnDestroy(): void {
+    this.productSub?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.productSub = this.cartService.products.subscribe((products) => {
+      this.amount = products.length;
+    });
+  }
+}
