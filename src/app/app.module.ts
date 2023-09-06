8c524b77a0f8e6ef6dbc4eaaca99d1ee66dcd4a4
Niko Pettersen <niko.pettersen@newthings.co>
2023-09-06 16:39:28 +0300

    Add service for handling the shopping cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ApiService } from './api.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartService } from './cart.service';
 import { HeaderComponent } from './header/header.component';
 import { LinkButtonComponent } from './link-button/link-button.component';
 import { LoggerService } from './logger.service';
@@ -22,6 +23,6 @@ import { SessionStorageService } from './session-storage.service';
     ProductListComponent,
   ],
   imports: [AppRoutingModule, BrowserModule, HttpClientModule],
-  providers: [ApiService, LoggerService, SessionStorageService],
+  providers: [ApiService, CartService, LoggerService, SessionStorageService],
 })
 export class AppModule {}
---
+++ src/app/cart.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+import { ProductSchema, type Product } from '../schemas';
+import { LoggerService } from './logger.service';
+import { SessionStorageService } from './session-storage.service';
+
+const KEY_CART_SERVICE = 'cart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private _products = new BehaviorSubject<Product[]>([]);
+
+  public products = this._products.asObservable();
+
+  constructor(
+    private logger: LoggerService,
+    private sessionStorage: SessionStorageService,
+  ) {
+    try {
+      this.logger.log('Attempting to restore cart from sessionStorage…');
+      const previousCart: Product[] = JSON.parse(
+        this.sessionStorage.getItem(KEY_CART_SERVICE) ?? '[]',
+      );
+      if (ProductSchema.array().safeParse(previousCart).success) {
+        this._products.next(ProductSchema.array().parse(previousCart));
+      }
+      this.logger.debug('Previous products:', this._products.value);
+      this.logger.info(
+        this._products.value.length
+          ? 'Cart restored successfully.'
+          : 'No previous cart detected.',
+      );
+    } catch (error) {
+      this.logger.info('Unable to restore previous cart.');
+      this.logger.error(error);
+      this.resetCart();
+    }
+  }
+
+  public addToCart(product: Product): void {
+    try {
+      const parsedProduct = ProductSchema.parse(product);
+      this.logger.debug('Adding product to cart:', parsedProduct);
+      this._products.next([...this._products.value, parsedProduct]);
+    } catch (error) {
+      this.logger.error('Unable to add product to cart:', product);
+      this.logger.error(error);
+    }
+    this.saveCart();
+  }
+
+  public removeFromCart(product: Product): void {
+    this._products.next(
+      this._products.value.filter((p) => p.id !== product.id),
+    );
+    this.saveCart();
+  }
+
+  public resetCart(): void {
+    this.sessionStorage.removeItem(KEY_CART_SERVICE);
+    this._products.next([]);
+  }
+
+  public saveCart(): void {
+    this.sessionStorage.setItem(KEY_CART_SERVICE, this._products.value);
+  }
+}
